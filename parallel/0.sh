580bef7e1494f246847adfda3e0f011d2ed3e47c
AlessioDP123 <alessiojuan.depaoli@studenti.unitn.it>
2021-11-19 12:02:20 +0100

    parallel folder

---
+++ parallel/0.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#PBS -l select=1:ncpus=1:mem=2gb
+# set max execution time
+#PBS -l walltime=0:01:00
+# imposta la coda di esecuzione
+#PBS -q short_cpuQ
+module load hdf5-1.10.5--gcc-9.1.0 netcdf-4.7.0--gcc-9.1.0
+/home/alessiojuan.depaoli/Project/parallel/read
