9ccd5d833430e27498f4b45146408d4c3f3fd455
Bralli99 <brando.chiminelli99@gmail.com>
2021-11-19 17:10:10 +0100

    add test folder with examples on wr and rd in paralled 4D data (pnetcdf library needed)

---
+++ parallel/test/test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#PBS -l select=4:ncpus=1:mem=2gb
+# set max execution time
+#PBS -l walltime=0:01:00
+# imposta la coda di esecuzione
+#PBS -q short_cpuQ
+module load mpich-3.2 hdf5-1.10.5--gcc-9.1.0 netcdf-4.7.0--gcc-9.1.0
+mpirun.actual -n 4 /home/brando.chiminelli/exercises/PnetCDF/test/pres_temp_4D_rd
